d732f3904aead53b5eb31f6615eb50b8673106f4
Gabriel Belle <gabrielbelle80@gmail.com>
2024-08-02 06:45:19 -0300

    add js

---
+++ mine_projeto/main.js
@@ -0,0 +1,8 @@
+var Leonardo = Window.document.getElementById(Leonardo)
+var Samantha = Window.document.getElementById(Samantha)
+var Bruna = Window.document.getElementById(Bruna)
+
+function RolarParaDireita() {
+    Leonardo.style = "display:none"
+    Bruna.style = "dispaly:flex"
+}
